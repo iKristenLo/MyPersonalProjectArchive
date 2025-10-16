1262f0c9d0842d28a493b85e4e26b47e046f2d5e
TheZyrovGroup <itsjezez@gmail.com>
2025-10-15 20:20:45 -0400

    a

---
+++ Python/2025/Unit1.4(CodePratice).py
@@ -0,0 +1,29 @@
+#1
+# def display_name():
+#     name = "Jordan"
+#     print("hello",name)
+# display_name()
+# print(name)
+
+#2
+# global message
+# message = "Outside Function"
+# def show_message():
+#     message = "Inside Function"
+#     print(message)
+# show_message()
+# print(message)
+
+#3
+# def add_numbers():
+#     total = 10 + 5
+#     print(total)
+# add_numbers()
+# print(total)
+
+#4
+global school
+school = "Molloy"
+def use_gloab():
+    print(school)
+use_gloab()
